9f1d5ac4d8fc8c6eed54407df231171fa2ee0cac
wuduozhi <wduozhi@gmail..com>
2018-09-30 19:37:35 +0800

    '添加习题模块'

---
+++ two/Practice.py
@@ -0,0 +1,137 @@
+# -*- coding:utf-8 -*-
+import sys
+import json
+import io
+import random
+
+# sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
+
+class Practice:
+	def __init__(self):
+		# self.op_primary = ["+","-","*","/"]
+		# self.op_middle = ["+","-","*","/","^0.5","^2"]
+		# self.op_high = ["+","-","*","/","^0.5","^2","cos","sin","tan"]
+		self.operation = {}
+		self.operation['primary'] = ["+","-","*","/"]
+		self.operation['middle'] = ["^0.5","","^2"]
+		self.operation['high'] = ["cos","sin","tan"]
+		# self.practices = []
+
+	def pratice_primary(self,count,type="primary"):
+		op_list = self.operation[type]
+		length = len(op_list)
+		practices = []
+		for i in range(0,count):
+			operand = random.randint(1,5) # 操作数
+			item = str(i) +": "                     # 题目
+			for j in range(0,operand):
+				num = random.randint(1,100)
+				op = random.randint(0,length-1)
+				string = str(num) +' ' + op_list[op] + ' '
+				item = item + string
+
+			num = random.randint(1,100)		
+			item = item + str(num) + ' ='
+			item = self.addBrackets(item)
+			practices.append(item)
+			print(item)
+		return practices
+
+	def practice_high(self,count,type="high"):
+		op_list = self.operation["primary"]
+		ext_list = self.operation["high"]
+		practices = []
+		length = len(op_list)
+		for i in range(0,count):
+			operand = random.randint(1,5) # 操作数
+			item = str(i) +": "                     # 题目
+			have = 0                      # 是否已经用特殊符号
+			for j in range(0,operand):
+				flag = random.randint(0,6)   # 可以调节生成特殊符号的概率
+				if flag < 3 :
+					num = ext_list[flag] + str(random.randint(1,100)) 
+					have = 1
+				else:
+					num = str(random.randint(1,100))
+
+				if num == "tan90":   # tan90度的bug
+					x = x-1
+					continue
+				op = random.randint(0,length-1)
+				string = str(num) +' ' + op_list[op] + ' '
+				item = item + string
+
+			num = random.randint(1,100)		
+			item = item + str(num) + ' ='
+			item = self.addBrackets(item)
+			practices.append(item)
+			print(item)
+		return practices
+
+	def practice_middle(self,count,type="middle"):
+		op_list = self.operation["primary"]
+		ext_list = self.operation["middle"]
+		practices = [] #保存结果
+		length = len(op_list)
+		for i in range(0,count):
+			operand = random.randint(1,5) # 操作数
+			item = str(i) +": "                     # 题目
+			have = 0                      # 是否已经用特殊符号
+			for j in range(0,operand):
+				flag = random.randint(0,6)   # 可以调节生成特殊符号的概率
+				if flag < 3 :
+					num = str(random.randint(1,100)) + ext_list[flag]
+					have = 1
+				else:
+					num = str(random.randint(1,100))
+				op = random.randint(0,length-1)
+				string = str(num) +' ' + op_list[op] + ' '
+				item = item + string
+
+			num = random.randint(1,100)		
+			item = item + str(num) + ' ='
+			item = self.addBrackets(item)
+			practices.append(item)
+			print(item)
+		return practices
+
+	def produce(self,mold,count):
+		if mold == "primary" :
+			return self.pratice_primary(count)
+		elif mold == "middle":
+			return self.practice_middle(count)
+		else:
+			return self.practice_high(count)
+
+	def addBrackets(self,item):   # 添加括号
+		num = random.randint(0,1)
+		string = item
+		for x in range(0,num):
+			length = len(string)
+			for j in range(0,length):
+				flag = random.randint(0,1)
+				if flag==1 and string[j] in self.operation['primary']:
+					# string = string[:j+1] + "(" + string[j+1:]
+					for y in range (j+1,len(string)):   # 找到下一个基本运算符
+						if string[y] in self.operation['primary']:
+							break
+
+					n = random.randint(y+1,len(string))
+					for z in range(n,len(string)):
+						if string[z] in self.operation['primary']:   # 一起加括号
+							string = string[:j+1] + "(" + string[j+1:]
+							string = string[:z] + ")" + string[z:]
+							break
+
+					break
+
+		return string
+
+
+
+
+if __name__ == '__main__':
+	p = Practice()
+	p.pratice_primary(count=5,type="primary")
+	# p.practice_middle(count=5)
+	# p.practice_high(count=5)
--- two/gui.py
+++ two/gui.py
@@ -1,55 +1,41 @@
-# -*- coding: UTF-8 -*-
-import tkinter as tk
 from tkinter import *
-from tkinter import ttk
-from tkinter import scrolledtext
-import sys
-import json
-import io
-import random
-
-
-class Gui(object):
-	
-	@staticmethod
-	def login():
-		# Create instance
-		window = tk.Tk()
-		# Add a title
-		window.title("Main GUI")
-		
-		# window.geometry("300x200")
-
-		app = Frame(window)
-		app.grid(column=0, row=0)
-		
-		label_phone = Label(app,text="phone :")
-		label_phone.grid(column=0, row=0)
-
-		label_passwd = Label(app,text="passwd :")
-		label_passwd.grid(column=0, row=1)
-
-
-		phone = StringVar()
-		entry_phone = Entry(app,textvariable = phone)
-		entry_phone.grid(column=1, row=0)
-
-		passwd = StringVar()
-		entry_passwd = Entry(app,textvariable = passwd)
-		entry_passwd.grid(column=1, row=1)
-
-		btn_register = Button(app)
-		btn_register.grid(column=0, row=2)
-		btn_register.configure(text="register")
-
-		btn = Button(app)
-		btn.grid(column=1, row=2)
-		#小部件的任何选项都可以通过configure()方法操作
-		btn.configure(text = "submit")
-
-		window.mainloop()
-
-if __name__ == '__main__':
-	phone = StringVar()
-	passwd = StringVar()
-	Gui.login()
+from tkinter.messagebox import *
+ 
+class LoginPage(Frame):
+    def __init__(self):
+        super().__init__()
+        self.username = StringVar()
+        self.password = StringVar()
+        self.pack()
+        self.createForm()
+ 
+    def createForm(self):
+        Label(self).grid(row=0, stick=W, pady=10)
+        Label(self, text = '账户: ').grid(row=1, stick=W, pady=10)
+        Entry(self, textvariable=self.username).grid(row=1, column=1, stick=E)
+        Label(self, text = '密码: ').grid(row=2, stick=W, pady=10)
+        Entry(self, textvariable=self.password, show='*').grid(row=2, column=1, stick=E)
+        Button(self, text='登陆', command=self.loginCheck).grid(row=3, stick=W, pady=10)
+        Button(self, text='退出', command=self.quit).grid(row=3, column=1, stick=E)
+ 
+    def loginCheck(self):
+        name = self.username.get()
+        secret = self.password.get()
+        if name=='wangliang' and secret=='123456':
+            self.destroy()
+            # MainPage()
+        else:
+            showinfo(title='错误', message='账号或密码错误！')
+            # print('账号或密码错误！')
+        
+root = Tk()
+root.title('小程序')
+width = 280
+height = 200
+screenwidth = root.winfo_screenwidth()  
+screenheight = root.winfo_screenheight() 
+alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth-width)/2, (screenheight-height)/2)
+root.geometry(alignstr)    # 居中对齐
+ 
+page1 = LoginPage()
+root.mainloop()
